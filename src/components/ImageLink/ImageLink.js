615e71365e3ac94513a91a3d2808e7b8227ff20a
Sridarshan Balaji <sridarshan1997@gmail.com>
2021-11-08 19:48:29 +0530

    Creating unit tests and attaching value of input to the input tag

--- src/components/ImageLink/ImageLink.js
+++ src/components/ImageLink/ImageLink.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./ImageLink.css";
 
-const ImageLink = ({ onInputChange, onButtonSubmit }) => {
+const ImageLink = ({ onInputChange, onButtonSubmit, inputValue }) => {
   return (
     <div className="input-container">
       <p>{`This Magic Brain will detect faces in your pictures. Give it a try !!`}</p>
       <div className="flex-container inputs">
         <input
           type="text"
+          value={inputValue}
           placeholder="Enter an URL"
           onChange={onInputChange}
         />
